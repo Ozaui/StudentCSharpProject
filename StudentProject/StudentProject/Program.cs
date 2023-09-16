b0f9a19373ceeed39111c7e23f543977d816af12
Özay Melih Yıldız <ozay.m.yildiz@gmail.com>
2023-09-17 00:11:09 +0300

    Initial commit

---
+++ StudentProject/StudentProject/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student(1, "Özay", "Yıldız", 70f, 60f, 85f, "SDU");
+
+            int option;
+            while (true) 
+            {
+                Console.WriteLine("Please choose one of the selection");
+                Console.WriteLine("1 => Show student information");
+                Console.WriteLine("2 => Calculate score");
+                Console.WriteLine("3 => show student school");
+                Console.WriteLine("4 => Exit");
+
+                option = Convert.ToInt32(Console.ReadLine());
+
+                if (option == 1) 
+                {
+                    student.showStudentInformation();
+                }else if(option == 2)
+                {
+                    student.calculateScore();
+                }else if (option == 3)
+                {
+                    student.showStudentSchool();
+                }else if(option == 4)
+                {
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StudentProject/StudentProject/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentProject
+{
+    internal class Student
+    {
+        private int studentNo;
+        private string studentName;
+        private string studentLastName;
+        private float vize1;
+        private float vize2;
+        private float final;
+        private string schoolName;
+
+        public Student(int studentNo, string studentName, string studentLastName, float vize1, float vize2, float final, string schoolName)
+        {
+            this.studentNo = studentNo;
+            this.studentName = studentName;
+            this.studentLastName = studentLastName;
+            this.vize1 = vize1;
+            this.vize2 = vize2;
+            this.final = final;
+            this.schoolName = schoolName;
+        }
+
+        public void showStudentInformation()
+        {
+            Console.WriteLine("Student no : " + studentNo);
+            Console.WriteLine("Student name : " + studentName);
+            Console.WriteLine("Student last name : " + studentLastName);
+            Console.WriteLine("Student first exam : " + vize1);
+            Console.WriteLine("Student second exam : " + vize2);
+            Console.WriteLine("Student final exam : " + final);
+            Console.WriteLine("Student school name : " + schoolName);
+        }
+        public void calculateScore()
+        {
+            Console.WriteLine("Student school score is " + ((vize1 * 0.3) + (vize2 * 0.3) + (final * 0.4)));
+        }
+        public void showStudentSchool()
+        {
+            Console.WriteLine("Student's school is " + schoolName);
+        }
+    }
+}
